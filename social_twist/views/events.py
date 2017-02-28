6ac64cc42cfa889e504bf87660295e5881499e15
Dmitrii Prihodco <dmitrii.prihodco@gmail.com>
2017-03-01 01:16:18 +0200

    Updated and documented events.

--- social_twist/views/events.py
+++ social_twist/views/events.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.decorators import list_route, detail_route
@@ -8,7 +9,7 @@ from django.contrib.gis.measure import Distance
 
 from social_twist.models import Event, Invitation
 from social_twist.serializers import EventSerializer, InvitationSerializer,\
-    PersonSerializer
+    PersonSerializer, FriendSerializer
 
 
 class EventView(viewsets.ModelViewSet):
@@ -16,41 +17,95 @@ class EventView(viewsets.ModelViewSet):
     serializer_class = EventSerializer
 
     def create(self, request, **kwargs):
+        """
+        This creates your events.
+        The fields should be pretty self explanatory.
+        """
         result = super(viewsets.ModelViewSet, self).create(request, **kwargs)
-        invites = request.data.get('friends', [])
+        invites = request.data.getlist('friends[]', [])
         for friend_id in invites:
+            receiver = User.objects.get(pk=friend_id)
             invitation = Invitation(sender=request.user,
-                                    receiver_id=int(friend_id),
-                                    event_id=result.data.id)
+                                    receiver=receiver,
+                                    event_id=result.data['id'])
             invitation.save()
         return result
 
     def list(self, request, *args, **kwargs):
+        """
+        This gives you a list of events.
+        - - -
+        It also takes several optional params as GET params:\n
+        __lat__ - latitude\n
+        __lon__ - longitude\n
+        __radius__ - radius in km from the point specified.\n
+        These 3 params together provide geographical filtering for the events.
+        As the server is using SRID 4326 for calculating distances, specification is needed:\n
+        -180 <= __lat__ <= 0 for the Western hemisphere
+        and 0 <= __lat__ <= 180 for the Eastern one.\n
+        Likewise -90 <= __lon__ <= 0 for the Southern hemisphere,
+        and 0 <= __lon__ <= 90 for the Northern hemisphere.\n
+
+        - - -
+        Additional optional parameter is:\n
+        __text__ - which enables a full text search through event fields.
+        """
         queryset = self.get_queryset()
-        lon = float(request.data.get('lon', 0))
-        lat = float(request.data.get('lat', 0))
-        radius = int(request.data.get('radius', 10))
-        point = Point(lon, lat)
-        queryset = queryset.filter(coordinates__distance_lt=(point, Distance(km=radius)))
-        queryset = queryset.filter(Q(is_private=False)|
-                                   Q(creator__in=request.user.info.friends))
+        lat = float(request.GET.get('lat', 0))
+        lon = float(request.GET.get('lon', 0))
+        radius = int(request.GET.get('radius', 10))
+        text = request.GET.get('text')
+        point = Point(lat, lon)
+        queryset = queryset.filter(coordinates__distance_lte=(point, Distance(km=radius)))
+        queryset = queryset.filter(Q(is_private=False) |
+                                   Q(creator__in=request.user.info.friends.all()))
+        if text is not None:
+            queryset.filter(Q(title__icontains=text) |
+                            Q(description__icontains=text) |
+                            Q(location__icontains=text) |
+                            Q(creator__first_name__icontains=text) |
+                            Q(creator__last_name__icontains=text))
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
+    def partial_update(self, request, pk=None, *args, **kwargs):
+        """
+        This is how you should update the event.
+        The patch method allows us to send to the server only those field that we desire to change.
+        - - -
+        Param:
+        __id__ - of the event that we going to update.
+        __body__ - of the request is of the same structure as we have in create method.
+        """
+        result = super(viewsets.ModelViewSet, self).partial_update(request, *args, **kwargs)
+        return result
+
     @list_route()
     def by_friends(self, request):
-        queryset = Event.objects.filter(creator__in=request.user.info.friends)
+        """
+        Shows events that were created by friends of the current user.
+        """
+        queryset = Event.objects.filter(creator__in=request.user.info.friends.all())
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     @list_route()
     def by_me(self, request):
+        """
+        Shows events that were created by the current user.
+        """
         queryset = Event.objects.filter(creator=request.user)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     def destroy(self, request, pk=None, *args, **kwargs):
-        event = Event.objects.get(pk=int(pk))
+        """
+        Allows creator to destroy his event.
+        - - -
+        Param:
+        __id__ - of the event that is about to be destroyed.
+        """
+        event = Event.objects.get(pk=pk)
         if event.creator == request.user:
             event.delete()
             return Response({"code": 1})
@@ -58,28 +113,39 @@ class EventView(viewsets.ModelViewSet):
 
     @detail_route()
     def attenders(self, request, pk=None):
-        event = Event.objects.get(pk=int(pk))
+        """
+        Returns a list of users that are going to the event specified.
+        - - -
+        Param:
+        __id__ - of the event that we are interested in.
+        """
+        event = Event.objects.get(pk=pk)
         serializer = PersonSerializer(event.attenders, many=True)
         return Response(serializer.data)
 
     @detail_route(methods=['post'])
     def attend(self, request, pk=None):
-        event = Event.objects.get(pk=int(pk))
+        """
+        Mark yourself as an attender to the event specified.
+        - - -
+        Param:
+        __id__ - of the event that we are interested in.
+        """
+        event = Event.objects.get(pk=pk)
         event.attenders.add(request.user)
         return Response({"code": 1})
-    # def retrieve(self, request, pk=None, *args, **kwargs):
-    #     event = Event.objects.get(pk=int(pk))
-    #     serializer =
 
 
 class InvitationView(viewsets.ModelViewSet):
     queryset = Invitation.objects.all()
     serializer_class = InvitationSerializer
 
+    def create(self, request, **kwargs):
+        result = super(viewsets.ModelViewSet, self).create(request, **kwargs)
+        return result
+
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset().filter(receiver=request.user)
-        # TODO Check the impact
-        queryset.update(seen=True)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
