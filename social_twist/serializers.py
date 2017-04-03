65c327d1e113a185cd236a49ecfad79b4b51f3a1
Dmitrii Prihodco <dmitrii.prihodco@gmail.com>
2017-04-03 22:52:00 +0300

    Forcing save of updated password.

--- social_twist/serializers.py
+++ social_twist/serializers.py
@@ -66,6 +66,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         user = super(UserSerializer, self).update(instance, validated_data)
         if 'password' in validated_data:
             instance.set_password(validated_data['password'])
+            instance.save()
         return user
 
 
