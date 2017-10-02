5195b271dfa7086374ec25664718dbbf149fde80
Dmitrii Prihodco <dmitrii.prihodco@gmail.com>
2017-10-02 22:23:09 +0300

    Updates to oauth2_provider.

--- social_twist/settings.py
+++ social_twist/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
@@ -143,5 +143,5 @@ OAUTH2_PROVIDER = {
     'OAUTH2_BACKEND_CLASS': 'oauth2_provider.oauth2_backends.JSONOAuthLibCore'
 }
 
-MEDIA_ROOT = "/home/twist/media/"
+MEDIA_ROOT = "/media/"
 MEDIA_URL = "http://%s/media/" % ALLOWED_HOSTS[0]
